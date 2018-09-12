efd8e9e03c7633d2aad89fa0adefb8a8cc2e2c8b
abhi-cognitivo <abhi@cognitivo.in>
2018-09-12 19:54:28 -0400

    First Commit

---
+++ DebeHaber.API/Models/Invoice.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DebeHaber.API.Models
+{
+    /// <summary>
+    /// Invoice types.
+    /// </summary>
+    public enum InvoiceTypes { 
+        /// <summary>
+        /// The purchase.
+        /// </summary>
+        Purchase = 1, 
+        /// <summary>
+        /// The purchase return.
+        /// </summary>
+        PurchaseReturn = 3, 
+        /// <summary>
+        /// The sales.
+        /// </summary>
+        Sales = 4, 
+        /// <summary>
+        /// The sales return.
+        /// </summary>
+        SalesReturn = 5 
+    }
+    
+    public enum BusineesCenter 
+    { 
+        /// <summary>
+        /// The revenue by service.
+        /// </summary>
+        RevenueByService = 1, 
+        /// <summary>
+        /// The asset inventory.
+        /// </summary>
+        Asset_Inventory = 2, 
+        /// <summary>
+        /// The fixed asset.
+        /// </summary>
+        FixedAsset = 3 
+    }
+
+    public class Invoice
+    {
+        /// <summary>
+        /// Gets or sets the type.
+        /// </summary>
+        /// <value>The type.</value>
+        public InvoiceTypes Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the local identifier.
+        /// </summary>
+        /// <value>The local identifier.</value>
+        public int Local_id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the cloud identifier.
+        /// </summary>
+        /// <value>The cloud identifier.</value>
+        public int Cloud_id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the customer tax identifier.
+        /// </summary>
+        /// <value>The customer tax identifier.</value>
+        public string CustomerTaxID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the customer.
+        /// </summary>
+        /// <value>The name of the customer.</value>
+        public string CustomerName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the supplier tax identifier.
+        /// </summary>
+        /// <value>The supplier tax identifier.</value>
+        public string SupplierTaxID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the supplier.
+        /// </summary>
+        /// <value>The name of the supplier.</value>
+        public string SupplierName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date.
+        /// </summary>
+        /// <value>The date.</value>
+        public string Date { get; set; }
+
+        /// <summary>
+        /// Gets or sets the code.
+        /// </summary>
+        /// <value>The code.</value>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number.
+        /// </summary>
+        /// <value>The number.</value>
+        public string Number { get; set; }
+
+        /// <summary>
+        /// Gets or sets the comment.
+        /// </summary>
+        /// <value>The comment.</value>
+        public string Comment { get; set; 
+
+        /// <summary>
+        /// Gets or sets the code expiry.
+        /// </summary>
+        /// <value>The code expiry.</value>
+        public string CodeExpiry { get; set; }
+
+        /// <summary>
+        /// Gets or sets the payment condition.
+        /// </summary>
+        /// <value>The payment condition.</value>
+        public int PaymentCondition { get; set; }
+
+        /// <summary>
+        /// Gets or sets the currency code.
+        /// </summary>
+        /// <value>The currency code.</value>
+        public string CurrencyCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the currency rate.
+        /// </summary>
+        /// <value>The currency rate.</value>
+        public decimal CurrencyRate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the details.
+        /// </summary>
+        /// <value>The details.</value>
+        public ICollection<InvoiceDetail> Details { get; set; }
+    }
+
+        /// <summary>
+        /// Invoice detail.
+        /// </summary>
+    public class InvoiceDetail
+    {
+            /// <summary>
+            /// Gets or sets the type.
+            /// </summary>
+            /// <value>The type.</value>
+        public BusineesCenter Type { get; set; }
+
+            /// <summary>
+            /// Gets or sets the VATP ercentage.
+            /// </summary>
+            /// <value>The VATP ercentage.</value>
+        public Int32 VATPercentage { get; set; }
+
+            /// <summary>
+            /// Gets or sets the value.
+            /// </summary>
+            /// <value>The value.</value>
+        public decimal Value { get; set; }
+
+            /// <summary>
+            /// Gets or sets the cost.
+            /// </summary>
+            /// <value>The cost.</value>
+        public decimal Cost { get; set; }
+
+            /// <summary>
+            /// Gets or sets the name.
+            /// </summary>
+            /// <value>The name.</value>
+        public string Name { get; set; }
+    }
+}
---
+++ DebeHaber.API/Send.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DebeHaber.API
+{
+    /// <summary>
+    /// Use this class to send your data.
+    /// </summary>
+    public class Send
+    {
+        /// <summary>
+        /// Send to.
+        /// </summary>
+        public enum SendTo { 
+            /// <summary>
+            /// The production.
+            /// </summary>
+            Production, 
+            /// <summary>
+            /// The playground.
+            /// </summary>
+            Playground 
+        }
+
+        private string API { get; set; }
+
+        private string Url { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:DebeHaber.API.Send"/> class.
+        /// </summary>
+        /// <param name="SendTo">URL.</param>
+        /// <param name="API_Key">API key.</param>
+        public Send(SendTo SendTo, string API_Key)
+        {
+            API = API_Key;
+            Url = SendTo == SendTo.Production ? "https://www.debehaber.com" : "https://test.debehaber.com";
+        }
+
+        /// <summary>
+        /// Invoices the specified Sales.
+        /// </summary>
+        /// <returns>List of Invoices with information of successful sync.</returns>
+        /// <param name="Invoices">List of Invoices which can include Sales, Purchases, Credit Notes, and Debit Notes.</param>
+        public List<Models.Invoice> Invoices(List<Models.Invoice> Invoices)
+        {
+
+            return Invoices;
+        }
+
+        /// <summary>
+        /// Accounts the movements.
+        /// </summary>
+        /// <returns>The movements.</returns>
+        /// <param name="AccountMovements">Purchases.</param>
+        public List<Models.Invoice> AccountMovements(List<Models.Invoice> AccountMovements)
+        {
+
+            return AccountMovements;
+        }
+    }
+}
